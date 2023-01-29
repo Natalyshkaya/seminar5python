076f31c77c006c3d25574ac50cdc7022f159015f
Nataly <naty.sh@mail.ru>
2023-01-29 14:58:41 +0500

    задача1 решение с файлом

--- задача1/задача1.py
+++ задача1/задача1.py
@@ -1,3 +1,5 @@
-text = input().split()
+with open('file.txt', 'r', encoding='utf-8') as file:
+    text = file.read().split()
+print(text)
 res_text = list(filter(lambda word: 'а' not in word and 'б' not in word and 'в' not in word, text))
 print(res_text)
